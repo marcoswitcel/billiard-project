6d30ea38862247b1159baf97b6ccf0cb483b8d13
Joao Marcos <joao.marcos.witcel@gmail.com>
2024-11-07 22:07:38 -0300

    solbing collision

--- src/physics-solver.js
+++ src/physics-solver.js
@@ -14,6 +14,7 @@ export class PhysicsSolver {
     // aplicando forças
     this.applyGravity();
     this.applyConstraint();
+    this.solveCollision();
     this.updatePositions(deltaTime);
   }
 
@@ -56,6 +57,34 @@ export class PhysicsSolver {
       {
         const n = to_obj.normalized();
         entity.currentPosition = position.copy().add(n.mul(radius - entity.shape.radius));
+      }
+    }
+  }
+
+  solveCollision()
+  {
+    const object_count = this.entities.length;
+
+    for (let i = 0; i < object_count; i++)
+    {
+      const entity1 = this.entities[i];
+
+      for (let j = i + 1; j < object_count; j++)
+      {
+        const entity2 = this.entities[j];
+        const collisionAxis = entity1.currentPosition.copy().sub(entity2.currentPosition);
+        const dist = collisionAxis.length();
+        const minDist = entity1.shape.radius + entity2.shape.radius;
+        if (dist < minDist)
+        {
+          const n = collisionAxis.copy().div(dist);
+          const delta = minDist - dist;
+
+          const result = 0.5 * delta;
+          
+          entity1.currentPosition = entity1.currentPosition.copy().add(n.copy().mul(result));
+          entity2.currentPosition = entity2.currentPosition.copy().sub(n.copy().mul(result));
+        }
       }
     }
   }
