f7fffefa2d28d71aa75fe4606804950619ae88f8
Joao Marcos <joao.marcos.witcel@gmail.com>
2024-11-09 17:01:26 -0300

    scenes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import { Circle } from './circle.js';
 import { CircleConstraint } from './constraints.js';
+import { DemonstrationScene, Scene01, Scene02 } from './demonstration-scene.js';
 import { Entity } from './entity.js';
 import { PhysicsSolver } from './physics-solver.js';
 import { drawCircle, drawRect } from './utils.js';
 import { vec2 } from './vec2.js';
 
 console.log('Olá mundo')
-
 
 const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d');
@@ -16,12 +16,9 @@ canvas.height = 500;
 
 document.body.append(canvas);
 
-const physicsSolver = new PhysicsSolver();
-//physicsSolver.gravity.set(0, 0);
-physicsSolver.constraints.push(new CircleConstraint(vec2(250, 200), 100));
-physicsSolver.entities.push(new Entity(vec2(265, 200), vec2(0, 0), new Circle(vec2(250, 200), 10, '#00F')));
-//physicsSolver.entities.push(new Entity(vec2(350, 200), vec2(0, 0), new Circle(vec2(250, 200), 10, '#F0F')));
-// physicsSolver.entities.push(new Entity(vec2(385, 200), vec2(0, 0), new Circle(vec2(250, 200), 10, '#F0F')));
+const scene = new Scene02(ctx);
+
+scene.setup();
 
 let lastTimestamp = 0;
 requestAnimationFrame(function loop(timestamp) {
@@ -35,37 +32,10 @@ requestAnimationFrame(function loop(timestamp) {
   const deltaTime = timestamp - lastTimestamp;
   const deltaTimeMs = deltaTime / 1000;
 
-  // background
-  drawRect(ctx, '#000', 0, 0, canvas.width, canvas.height);
+  scene.update(deltaTimeMs);
 
-  for (const constraint of physicsSolver.constraints) {
-    if (constraint instanceof CircleConstraint) {
-      const position = constraint.position;
-      const radius = constraint.radius;
-      const color = '#0F0';
-      drawCircle(ctx, position.x, position.y, radius, color);
-    }
-  }
-
-  /**
-   * @todo João, implementar o sistema de 'sub-steps' no update das entidades.
-   * @note tentei implementar um mecanismo simples, onde atualizo 3 vezes passando o deltaTimeMs dividido por três;
-   * dessa forma ficou mais estável a simualação.
-   */
-  physicsSolver.update(deltaTimeMs);
-
-  for (const entity of physicsSolver.entities) {
-    drawCircle(ctx, entity.currentPosition.x, entity.currentPosition.y, entity.shape.radius, entity.shape.color);
-  }
+  scene.render();
   
   lastTimestamp = timestamp;
-});
-
-document.addEventListener('keyup', event => {
-  if (event.key === ' ') {
-    const radius = Math.random() * 5 + 5;
-    const color = `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
-    physicsSolver.entities.push(new Entity(vec2(265, 200), vec2(0, 0), new Circle(vec2(250, 200), radius, color)));
-  }
 });
 
