940ad83c1bf128b586ff4bb748a6b6fc07bd7c7d
Joao Marcos <joao.marcos.witcel@gmail.com>
2024-11-10 22:17:42 -0300

    WIP: click to shoot

--- src/scene02.js
+++ src/scene02.js
@@ -34,20 +34,31 @@ export class Scene02 extends DemonstrationScene {
     this.physicsSolver.entities.push(new Entity(vec2(450, 185), vec2(0, 0), new Circle(vec2(250, 200), 10, '#F0F')));
     this.physicsSolver.entities.push(new Entity(vec2(450, 215), vec2(0, 0), new Circle(vec2(250, 200), 10, '#F0F')));
 
+    const shootForce = 5;
+
     document.addEventListener('keyup', event => {
       if (event.key === ' ') {
-        ball.currentPosition.add(vec2(5, 0));
+        ball.currentPosition.add(vec2(shootForce, 0));
       }
     });
 
     const canvas = this.ctx.canvas;
 
     canvas.addEventListener('click', event => {
+      /**
+       * @todo João, quando devidamente separado os eixos da simulação e da tela, será necessário calcular e 'projetar'
+       * o click no ponto correto.
+       */
       const boundings = canvas.getBoundingClientRect();
       // posição menos offset do canvas e reescalado para compensar o escalonamento atual do canvas
       const coords = { x: (event.clientX - boundings.x) / canvas.clientWidth, y: (event.clientY - boundings.y) / canvas.clientHeight, };
 
-      console.log('Click capturado: ', coords);
+      const force = vec2(event.clientX - boundings.x, event.clientY - boundings.y)
+        .sub(ball.currentPosition)
+        .normalize()
+        .mul(shootForce);
+
+      ball.currentPosition.add(force);
     });
   }
 
