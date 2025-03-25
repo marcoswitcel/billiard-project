afddddada51e8f2c0dc012d5fb4974526334ba55
Joao Marcos <joao.marcos.witcel@gmail.com>
2025-03-24 22:18:15 -0300

    WIP: color by name

--- src/color.js
+++ src/color.js
@@ -1,4 +1,9 @@
 
+const canvas = document.createElement('canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = 1;
+canvas.height = 1;
+
 export class Color {
   r = 0;
   g = 0;
@@ -9,11 +14,11 @@ export class Color {
     this.r = r;
     this.g = g;
     this.b = b;
-    this.a = a;
+    this.a = Math.min(a, 1);
   }
 
   copy() {
-    return new Color( this.r, this.g, this.b, this.a);
+    return new Color(this.r, this.g, this.b, this.a);
   }
 
   darken(value) {
@@ -25,5 +30,20 @@ export class Color {
 
   toString() {
     return `rgba(${this.r}, ${this.g}, ${this.b}, ${this.a})`;
+  }
+
+  /**
+   * @todo João, testar
+   * @param {string} color 
+   * @returns 
+   */
+  static from(color) {
+    ctx.rect(0, 0, 1, 1);
+    ctx.fillStyle = color;
+    ctx.fill();
+    const image = ctx.getImageData(0, 0, 1, 1);
+    const data = image.data;
+
+    return new Color(data[0], data[1], data[2], data[3]);
   }
 }
